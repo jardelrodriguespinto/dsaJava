a7a9bb4b027a746dceca7627651dabb07444c162
jardelrodriguespinto <ghmagno910@gmail.com>
2024-10-19 21:17:27 -0300

    Apendendo mais sobre recursividade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,18 @@ import binarysearchtrees.Node;
 public class Main {
     public static void main(String[] args) {
 
-        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
-        binarySearchTree.insert(1);
-        binarySearchTree.insert(2);
-        binarySearchTree.insert(3);
+//        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+//        binarySearchTree.insert(1);
+//        binarySearchTree.insert(2);
+//        binarySearchTree.insert(3);
+//
+        int[] nums = new int[]{1,2,3};
 
-        System.out.println(binarySearchTree);
+        System.out.println(imprimirNumerosIntevalo(nums, 0));
 
     }
+
+
+
+
 }
--- src/main/java/recursion/Recursion.java
+++ src/main/java/recursion/Recursion.java
@@ -2,8 +2,65 @@ package recursion;
 
 public class Recursion
 {
-    public static String isPalindrome(String word)
+    public static int regressiva(int num)
     {
-        return null;
+        if (num == 0)
+            return 0;
+
+        System.out.println(num);
+
+        return regressiva(num - 1);
+    }
+
+    public static int imprimePalavaraNVezes(String palavra, int qtd)
+    {
+        if (qtd == 0)
+            return 0;
+
+        System.out.println(palavra);
+
+        return imprimePalavaraNVezes(palavra, qtd - 1);
+    }
+
+    public static int somaTodosNumerosAteN(int num)
+    {
+        if (num == 0)
+            return 0;
+
+        return num + somaTodosNumerosAteN(num - 1);
+    }
+
+    public static int somarTodosNumerosArray(int[] arr, int sum)
+    {
+        if (sum > arr.length - 1)
+            return 0;
+
+        return  arr[sum] + somarTodosNumerosArray(arr, ++sum);
+    }
+
+    public static boolean contains(String word, Character target, int sum)
+    {
+        if (sum > word.length() - 1)
+            return false;
+        else if (word.charAt(sum) == target)
+            return true;
+
+        return (word.charAt(sum) == target) && contains(word, target, ++sum);
+    }
+
+    public static int count(String word, int num, int sum)
+    {
+        if (sum >= word.length())
+            return 0;
+
+        return  num + count(word, num, ++sum);
+    }
+
+    public static int biggest(int[] arr, int num)
+    {
+        if (num == arr.length)
+            return Integer.MIN_VALUE;
+
+        return Math.max(arr[num], biggest(arr, num + 1));
     }
 }
